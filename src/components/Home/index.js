81f185ea8a5c14a083c2b7240ee46cc2380bae57
arthurscorreia <arthurcorreia2703@gmail.com>
2025-06-06 20:54:00 -0300

    adicao de botao para playlist

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -12,12 +12,23 @@ const Home = () => {
             <h2>Esse site foi feito para você, com muito amor e carinho!</h2>
             <p>Teste os seus conhecimentos sobre o nosso namoro 👇!</p>
             <Link to="/quiz" className="botaoQuiz">
-                Quiz      
+                Quiz 🧠    
             </Link>
             <p>Veja a nossa galeria de fotos 👇!</p>
             <Link to="/galeria" className="botaoGaleria">
-                Galeria
+                Galeria 🧑🏻‍❤️‍👩🏻
             </Link>
+            <p>Ou ouça a nossa playlist no Spotify 👇!</p>
+            <a
+                href="https://open.spotify.com/playlist/7tDqn0eobUatUoNVWO94m6?si=480185711ccb444a&pt=d9b13df3c369e1aca4337fc724180b54"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                <button className="botaoPlaylist">
+                    {/* <img src="/images/spotify-icon.png" alt="Spotify Icon" /> */}
+                    Nossa playlist ❤️
+                </button>
+            </a>
             {/* <Link to="/galeria">
                 <div className = "botaoGaleria">
                 <button>Nossa galeria</button>
